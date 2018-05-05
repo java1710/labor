80d9db0e1d89de7968e8d9a3f2d78f171b59ee06
13603642094 <5624935@qq.com>
2018-05-05 17:04:06 +0800

    增加地区三级连动功能；

--- labor/src/main/java/com/oracle/labor/web/CommonHandler.java
+++ labor/src/main/java/com/oracle/labor/web/CommonHandler.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.oracle.labor.common.codetable.LanguageOperation;
 import com.oracle.labor.common.codetable.NationOperation;
+import com.oracle.labor.common.codetable.RegioncodeOperation;
 
 @Controller
+@RequestMapping("/service")
 public class CommonHandler {
 
 	//返回文本;
 	@ResponseBody
-	@RequestMapping(value="/service/language",produces="text/html;charset=UTF-8")
+	@RequestMapping(value="/language",produces="text/html;charset=UTF-8")
 	public String getLanguage(String val) {
 		String result=LanguageOperation.getOption(val);
 		System.out.println(result);
@@ -24,9 +26,25 @@ public class CommonHandler {
 	 * 获得民族的下拉列表 ;
 	 */
 	@ResponseBody
-	@RequestMapping(value="/service/nations/{value}",produces="text/html;charset=UTF-8")
+	@RequestMapping(value="/nations/{value}",produces="text/html;charset=UTF-8")
 	public String getNations(@PathVariable("value") String value) {
 		return NationOperation.getOption(value);
+	}
+	
+	/**
+	 * 获得地区的下拉列表 
+	 * @param region:区域类别，province,city,village
+	 * @param code:被选中的地区编码
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/regions",produces="text/html;charset=UTF-8")
+	public String getRegions(String region,String code) {
+		if(code==null||"".equals(code)) {
+			return RegioncodeOperation.getProvince();
+		}else {
+			return RegioncodeOperation.getSelectedRegion(code,region);
+		}		
 	}
 	
 }
--- labor/src/main/webapp/service/test/user.jsp
+++ labor/src/main/webapp/service/test/user.jsp
@@ -13,6 +13,28 @@
 		
 		//加载民族 
 		$("#nation").load("../nations/51");
+		
+		
+		//加载省份;
+		$("#province").load("../regions");
+
+		//省的改变事件
+		$("#province").change(function(){
+			$("#city").load("../regions",{code:$(this).val(),region:"city"});
+			$("#village").empty();
+		});
+		//市区change事件 
+		$("#city").change(function(){
+			$("#village").load("../regions",{code:$(this).val(),region:"village"});
+		});
+		
+		//回显按钮事件 
+		$("#btn_code").click(function(){
+			var code=$("#code").val();
+			$("#province").load("../regions",{code:code,region:"province"});
+			$("#city").load("../regions",{code:code,region:"city"});
+			$("#village").load("../regions",{code:code,region:"village"});
+		});
 	});
 </script>
 </head>
@@ -28,6 +50,13 @@
 	语言<select id="language" style="width: 150px"></select>
 	
 	
-	民族<select id="nation" style="width:150px"></select>
+	民族<select id="nation" style="width:150px"></select><br/>
+	
+	
+	<input type="text" id="code"/><input type="button" id="btn_code" value="提交"/><br/>
+	
+	<select id="province" style="width:150px"></select>省
+	<select id="city" style="width:150px"></select>市
+	<select id="village" style="width:150px"></select>区
 </body>
 </html>
